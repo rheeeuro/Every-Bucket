c941eba5879f499a2fbd25ce1feab6e45bbbc426
eurohand <eurohand@naver.com>
2019-06-30 19:31:30 +0900

    Custom Font (BM)

--- Header.js
+++ Header.js
@@ -2,22 +2,37 @@ import React, { Component } from 'react';
 import { Header } from 'react-native-elements';
 import { StyleSheet, Dimensions, View, TouchableOpacity } from 'react-native';
 import { Ionicons } from "@expo/vector-icons";
+import * as Font from 'expo-font'
 
 
 const { height, width } = Dimensions.get("window");
 
 export default class HeaderComponent extends Component {
-    render() {
-        return (
-          <Header 
-            placement="left"
-            leftComponent={{ icon: 'menu', color: '#273BE7' }}
-            centerComponent={{ text: '모두의 Bucket', style: { color: '#273BE7', fontSize: 20 } }}
-            rightComponent={ <RightComponent /> }
-            containerStyle={{ backgroundColor: '#fcfcfc' }}
-          />
-        );
-    }
+  state = {
+    fontLoaded: false
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'BMHANNA': require('./assets/fonts/BMHANNA_11yrs_ttf.ttf')
+    });
+    this.setState({ fontLoaded: true });
+  }
+
+  render() {
+      return (
+        <Header 
+          placement="left"
+          leftComponent={{ icon: 'menu', color: '#6675FF' }}
+          centerComponent={this.state.fontLoaded ? ({ 
+            text: '모두의 Bucket', 
+            style: { color: '#6675FF', fontSize: 20, fontFamily: 'BMHANNA' } 
+          }) : null}
+          rightComponent={ <RightComponent /> }
+          containerStyle={{ backgroundColor: '#fcfcfc' }}
+        />
+      );
+  }
 }
 
 class RightComponent extends Component {
@@ -25,13 +40,12 @@ class RightComponent extends Component {
     return (
       <View style={{flexDirection: "row"}}>
         <TouchableOpacity style={styles.icons}>
-          <Ionicons color="#273BE7" size={30} name="ios-search" />
+          <Ionicons color="#6675FF" size={30} name="ios-search" />
         </TouchableOpacity>
         <TouchableOpacity style={styles.icons}>
-          <Ionicons color="#273BE7" size={30} name="ios-home" />
+          <Ionicons color="#6675FF" size={30} name="ios-home" />
         </TouchableOpacity>
       </View>
-      
     );
   }
 }
